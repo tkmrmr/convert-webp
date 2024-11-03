ac534ddca68f788bc06a209ff5530b6c71dd3d2b
tkmrmr <dach1024@yahoo.co.jp>
2024-11-03 19:17:23 +0900

    update: filepathを引数に

--- convert_to_webp.py
+++ convert_to_webp.py
@@ -1,14 +1,15 @@
 from PIL import Image
 import os
 
-def convert_to_webp():
-    filepath = input("Enter the file path: ").replace("'", "")
+def convert_to_webp(filepath: str) -> None:
     dirname = os.path.dirname(filepath)
     filename = os.path.splitext(os.path.basename(filepath))[0]
 
     img = Image.open(filepath)
     img.save(os.path.join(dirname, filename + ".webp"), "webp")
 
-if __name__ == "__main__":
-    convert_to_webp()
+if __name__ == "__main__": 
+    filepath = input("Enter the file path: ").replace("'", "")
+    convert_to_webp(filepath)
+
     
