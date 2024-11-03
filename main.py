812cb6bd72207addf4daa9e71f3cf88236cb3bce
tkmrmr <dach1024@yahoo.co.jp>
2024-11-04 03:37:31 +0900

    change UI a little

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def main(page: ft.Page):
     page.title = "WebP変換ツール"
     page.window.width = 600
     page.window.height = 450
-    page.theme = ft.Theme(color_scheme_seed="white")
+    page.theme = ft.Theme()
 
     filepath_display = ft.Text("", visible=False)
     selected_image = ft.Image(
-        src="", fit=ft.ImageFit.SCALE_DOWN, visible=False
+        src="", fit=ft.ImageFit.SCALE_DOWN, visible=False,
     )
     compression_message = ft.Text("", visible=False)
+    progress_ring = ft.ProgressRing(visible=False)
 
     def pick_files_result(e: ft.FilePickerResultEvent):
         if e.files:
@@ -22,7 +23,7 @@ def main(page: ft.Page):
             filepath_display.visible = True
             selected_image.src = filepath
             selected_image.visible = True
-            convert_button.visible = True
+            convert_button.disabled = False
             page.update()
         else:
             pass
@@ -34,41 +35,54 @@ def main(page: ft.Page):
         "ファイルを選択",
         icon=ft.icons.FILE_OPEN,
         on_click=lambda _: pick_files_dialog.pick_files(allowed_extensions=["png", "jpg", "jpeg"], allow_multiple=False),
+        disabled=False,
     )
 
     def convert_button_clicked(e):
+        file_select_button.disabled = True
+        convert_button.disabled = True
+        progress_ring.visible = True
+        page.update()
         converted_image = convert_to_webp(selected_image.src)
         selected_image_size = os.path.getsize(selected_image.src)
         convered_image_size = os.path.getsize(converted_image)
         compression_rate = round((1 - convered_image_size / selected_image_size) * 100)
-        compression_message.value = f"{compression_rate}％削減されました"
+        compression_message.value = f"{compression_rate}％削減されました！"
         compression_message.visible = True
+        file_select_button.disabled = False
+        convert_button.disabled = False
+        progress_ring.visible = False
         page.update()
 
     convert_button = ft.ElevatedButton(
         "WebPに変換",
         icon=ft.icons.FILE_DOWNLOAD,
         on_click=convert_button_clicked,
-        visible=False,
+        disabled=True,
     )
 
     page.add(
         ft.Column(
             [
                 # ft.Text("WebPに変換する画像を選択"),
-                ft.Row(
-                    [file_select_button, filepath_display],
-
-                ),
+                ft.Row([
+                    ft.Row(
+                        [file_select_button, filepath_display],
+                    ),
+                    compression_message,
+                ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
                 ft.Container(
-                    content=selected_image,
+                    content=ft.Stack(
+                        [selected_image, progress_ring], 
+                        alignment=ft.alignment.center,
+                    ), 
                     height=280,
                     width=580,
                     padding=10,
                     border=ft.border.all(1),
+                    margin=ft.margin.only(bottom=5),
                 ),
                 convert_button,
-                compression_message,
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
